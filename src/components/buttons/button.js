000e383a9b9ced7b5aaf75008f6707ec9a2cddf9
farid-ad <faridaboudiab123@gmail.com>
2021-10-22 17:37:36 +0300

    update

---
+++ src/components/buttons/button.js
@@ -0,0 +1,12 @@
+import react from 'react'
+const Button = () => {
+    return (
+        <div>
+            <form action={action}>
+                <button></button>
+            </form>
+            <style jsx>{``}</style>
+        </div>
+    )
+}
+export default Button
